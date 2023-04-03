f9343b0f0f40925effcc7f793d67ccc3b7b76f10
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-04-03 22:36:04 +0200

    Api repositories 1

--- src/main/java/com/exercise/springRepositories/Entities/Car.java
+++ src/main/java/com/exercise/springRepositories/Entities/Car.java
@@ -20,4 +20,46 @@ public class Car {
 
     @Column
     private double currentPrice;
+
+    public Car() {
+    }
+
+    public Car(int id, String modelType, String serialNumber, double currentPrice) {
+        this.id = id;
+        this.modelType = modelType;
+        this.serialNumber = serialNumber;
+        this.currentPrice = currentPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelType() {
+        return modelType;
+    }
+
+    public void setModelType(String modelType) {
+        this.modelType = modelType;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
 }

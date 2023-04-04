335e79c6b98b5b94eaa6480f80f0c901416ae040
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-04-04 13:04:32 +0200

    Api repositories 2

--- src/main/java/com/exercise/springRepositories/Repositories/programRepository.java
+++ src/main/java/com/exercise/springRepositories/Repositories/programRepository.java
@@ -2,9 +2,10 @@ package com.exercise.springRepositories.Repositories;
 
 import com.exercise.springRepositories.Entities.ProgrammingLanguage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "repo-prog-languages", collectionResourceDescription = @Description("Repository for ProgrammingLanguage table"))
 public interface programRepository extends JpaRepository<ProgrammingLanguage,Integer> {
 
 
